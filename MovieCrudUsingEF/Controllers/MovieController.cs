9fd315db6b3f4ca16a2ecaae6be8a1556bcb8859
SnehalS26 <Snehalshirsath26@gmail.com>
2023-09-09 22:36:07 +0530

    Add project files.

---
+++ MovieCrudUsingEF/Controllers/MovieController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieCrudUsingEF.Models;
+
+namespace MovieCrudUsingEF.Controllers
+{
+    public class MovieController : Controller
+    {
+        ApplicationDbContext context;
+        MovieCrud moviecrud;
+        public MovieController(ApplicationDbContext context)
+        {
+            this.context = context;
+            moviecrud = new MovieCrud(this.context);
+        }
+        // GET: MovieController
+        public ActionResult Index()
+        {
+            return View(moviecrud.GetAllMovies());
+        }
+
+        // GET: MovieController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(moviecrud.GetMovieById(id));
+        }
+
+        // GET: MovieController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Movie movie)
+        {
+            try
+            {
+                var res = moviecrud.AddMovie(movie);
+                if(res== 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(moviecrud.GetMovieById(id));
+        }
+
+        // POST: MovieController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Movie movie)
+        {
+            try
+            {
+                var res = moviecrud.UpdateMovie(movie);
+                if (res == 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(moviecrud.GetMovieById(id));
+        }
+
+        // POST: MovieController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                var res = moviecrud.DeleteMovie(id);
+                if (res == 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MovieCrudUsingEF/Models/MovieCrud.cs
@@ -0,0 +1,55 @@
+﻿namespace MovieCrudUsingEF.Models
+{
+    public class MovieCrud
+    {
+        ApplicationDbContext context;
+        public MovieCrud(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return context.Movies.Where(x => x.IsActive == 1).ToList();
+        }
+        public Movie GetMovieById(int id)
+        {
+            var movie = context.Movies.Where(x => x.Id == id).FirstOrDefault();
+            return movie;
+        }
+        public int AddMovie(Movie movie)
+        {
+            movie.IsActive = 1;
+            int result = 0;
+            context.Movies.Add(movie);
+            result = context.SaveChanges();
+            return result;
+        }
+        public int UpdateMovie(Movie movie)
+        {
+            int result = 0;
+            var m = context.Movies.Where(x => x.Id == movie.Id).FirstOrDefault();
+            if (m != null)
+            {
+                m.Id = movie.Id;
+                m.Name = movie.Name;
+                m.Genere = movie.Genere;
+                m.Released_Date = movie.Released_Date;
+                m.Starcast = movie.Starcast;
+                m.IsActive = 1;
+                result = context.SaveChanges();
+            }
+            return result;
+        }
+        public int DeleteMovie(int id)
+        {
+            int result = 0;
+            var m = context.Movies.Where(x => x.Id == id).FirstOrDefault();
+            if (m != null)
+            {
+               m.IsActive = 0;
+                result = context.SaveChanges();
+            }
+            return result;
+        }
+    }
+}
